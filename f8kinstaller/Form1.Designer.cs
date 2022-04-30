becb8e73bffc9a6ca77a429a381e98781f02d698
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-04-30 19:42:34 +0200

    installer source code

---
+++ f8kinstaller/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace f8kinstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.messageloader = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lablhider = new System.Windows.Forms.Timer(this.components);
+            this.imgshow = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.timerdwa = new System.Windows.Forms.Timer(this.components);
+            this.thanks = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tha = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.thanks.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(82, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Piotrek4Hack";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(89, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "INSTALL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 299);
+            this.progressBar1.Maximum = 1020;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(339, 61);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(82, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enjoy the rat!";
+            this.label2.Visible = false;
+            // 
+            // messageloader
+            // 
+            this.messageloader.Interval = 5;
+            this.messageloader.Tick += new System.EventHandler(this.messageloader_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(394, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(241, 160);
+            this.label3.TabIndex = 4;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(399, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(234, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Installation Succeed!";
+            // 
+            // lablhider
+            // 
+            this.lablhider.Tick += new System.EventHandler(this.lablhider_Tick);
+            // 
+            // imgshow
+            // 
+            this.imgshow.Tick += new System.EventHandler(this.imgshow_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::f8kinstaller.Properties.Resources.arrow01;
+            this.pictureBox2.Location = new System.Drawing.Point(223, 120);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(69, 46);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::f8kinstaller.Properties.Resources.greenok;
+            this.pictureBox1.Location = new System.Drawing.Point(-220, 139);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(12, 124);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 169);
+            this.panel1.TabIndex = 8;
+            this.panel1.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(65, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(227, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "want to download faster?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(34, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(258, 40);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "consider                   to Piotrek4Hack\r\nfor a faster downloading speed!";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.linkLabel1.Location = new System.Drawing.Point(101, 77);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(71, 20);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "donating";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // timerdwa
+            // 
+            this.timerdwa.Interval = 10;
+            // 
+            // thanks
+            // 
+            this.thanks.Controls.Add(this.button2);
+            this.thanks.Controls.Add(this.label7);
+            this.thanks.Controls.Add(this.tha);
+            this.thanks.Location = new System.Drawing.Point(12, 121);
+            this.thanks.Name = "thanks";
+            this.thanks.Size = new System.Drawing.Size(352, 169);
+            this.thanks.TabIndex = 11;
+            this.thanks.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(101, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(238, 60);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "please follow instruction on the\r\nwebsite to securely pay and get\r\nthe faster Pio" +
+    "trek4Hack installer!";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // tha
+            // 
+            this.tha.AutoSize = true;
+            this.tha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tha.ForeColor = System.Drawing.Color.Yellow;
+            this.tha.Location = new System.Drawing.Point(241, 31);
+            this.tha.Name = "tha";
+            this.tha.Size = new System.Drawing.Size(98, 29);
+            this.tha.TabIndex = 8;
+            this.tha.Text = "Thanks!";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(259, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "dismiss";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.Location = new System.Drawing.Point(89, 366);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(190, 33);
+            this.exitbtn.TabIndex = 12;
+            this.exitbtn.Text = "CANCEL";
+            this.exitbtn.UseVisualStyleBackColor = true;
+            this.exitbtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(398, 323);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(235, 37);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "CLOSE INSTALLER";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(376, 444);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.thanks);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Piotrek4Hack Installer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.thanks.ResumeLayout(false);
+            this.thanks.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer messageloader;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer lablhider;
+        private System.Windows.Forms.Timer imgshow;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timerdwa;
+        private System.Windows.Forms.Panel thanks;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label tha;
+        private System.Windows.Forms.Button exitbtn;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ f8kinstaller/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace f8kinstaller
+{
+    public partial class Form1 : Form
+    {
+        public bool canshowpromo = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        public bool canload;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Show();
+            canload = true;
+            exitbtn.Hide();
+        }
+
+        Random rnd = new Random();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(canload == true)
+            {
+                if(progressBar1.Value < 1000)
+                {
+                    progressBar1.Value += rnd.Next(1, 11);
+                    timer1.Interval = rnd.Next(457, 999);
+
+                    if(progressBar1.Value > 400 && progressBar1.Value < 950)
+                    {
+                        if(canshowpromo == true)
+                        {
+                            panel1.Show();
+                        }
+                        
+                    }
+                    else
+                    {
+                        panel1.Hide();
+                    }
+                }
+                else
+                {
+                    label2.Show();
+                    button1.Hide();
+                    progressBar1.Hide();
+                    messageloader.Start();
+                    panel1.Hide();
+                    dismissthanks();
+                    canload = false;
+                }
+            }
+        }
+
+        int resizetimes = 100;
+
+        private void messageloader_Tick(object sender, EventArgs e)
+        {
+            if(resizetimes > 0)
+            {
+                //modify the window size
+                this.Width += 2;
+
+                resizetimes--;
+            }
+            else
+            {
+                messageloader.Stop();
+                lablhider.Start();
+                //download();
+                //seconddownload();
+                //HttpDownloadFileAsync(new HttpClient(), "", "");
+                //download();  
+            }
+        }
+
+        public int velocity = 1;
+
+        private void lablhider_Tick(object sender, EventArgs e)
+        {
+            label1.Left -= velocity;
+            label2.Left -= velocity;
+
+            velocity = ((velocity * 2) - (int)(velocity/2)) - (int)(velocity / 3);
+
+            if(velocity >= 400)
+            {
+                lablhider.Stop();
+                imgshow.Start();
+            }
+        }
+
+        public int picvel = 1; //picture velocity
+
+        private void imgshow_Tick(object sender, EventArgs e)
+        {
+            if(pictureBox1.Left < 65)
+            {
+                pictureBox1.Left += picvel;
+                picvel = ((picvel * 2) - (int)(picvel / 2)) - (int)(picvel / 3);
+            }
+            else
+            {
+                
+                imgshow.Stop(); //done!
+                seconddownload();
+
+            }
+
+            //Console.WriteLine("siema: " + pictureBox1.Left + ";       picvel = " + picvel);
+            
+        }
+
+        private void download()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://piotrekunitydeveloper.github.io/piotrek4.hack/testfile.txt", System.Environment.SpecialFolder.MyDocuments + "\\piotrek4hack.txt");
+                }
+
+                MessageBox.Show("Client Downloaded!");
+            }
+            catch
+            {
+                MessageBox.Show("ERROR: couldnt download file package from the server. Make sure you have internet access", "DOWNLOADING ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+            thankyou();
+        }
+
+        private void thankyou()
+        {
+            panel1.Hide();
+            canshowpromo = false;
+            thanks.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dismissthanks();
+        }
+
+        private void dismissthanks()
+        {
+            panel1.Hide();
+            canshowpromo = false;
+            thanks.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void seconddownload()
+        {
+            System.Diagnostics.Process.Start("https://drive.google.com/uc?export=download&id=1YIUg4ocj-2riN1VWtWQBsXlphPlgGGnq");
+        }
+
+        static public async Task HttpDownloadFileAsync(HttpClient httpClient, string url, string fileToWriteTo)
+        {
+            
+        }
+    }
+}
